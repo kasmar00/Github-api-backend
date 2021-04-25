f317734fed8d09c0bb818a83c7275970569f0024
Marcin_Kasznia <kasmar00@gmail.com>
2021-04-25 12:23:23 +0200

    documenting api classes

--- app/app.py
+++ app/app.py
@@ -1,15 +1,12 @@
-from os import environ
-
 from flask import Flask
 from flask_restful import Api
 
 from .resources import Stars, List
 
+# creating flask app and api
 app = Flask(__name__)
 api = Api(app)
 
+# adding resources to api
 api.add_resource(Stars, '/stars')
 api.add_resource(List, '/list')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=environ.get('flask_debug', False))
--- app/resources.py
+++ app/resources.py
@@ -1,6 +1,6 @@
 import json
 import urllib.request
-import re
+import typing
 
 
 from flask_restful import Resource
@@ -8,8 +8,26 @@ from flask import request
 
 
 class github(Resource):
+    """
+    Class for storing functions used by all routes
+
+    Those include:
+        downloading data from api,
+        extracting data,
+        handling exceptions
+    """
+
     @staticmethod
-    def getUrl(username):
+    def getUrl(username: typing.AnyStr) -> typing.List[typing.Dict]:
+        """
+        Downloads data from api
+
+        Parameters:
+            username - username (github handle) for which the data will be downloaded
+
+        Returns:
+            data - list of dictionaries downloaded from github api
+        """
         # if not re.match("^([a-z\d]+-)*[a-z\d]+$", username):
         if "" == username or " " in username or "/" in username:
             raise Exception("Bad username")
@@ -21,7 +39,20 @@ class github(Resource):
             raise Exception("User not found")
 
     @staticmethod
-    def extractData(data):
+    def extractData(data: typing.List[typing.Dict]) -> typing.List[typing.Dict]:
+        """
+        Extracts downloaded data
+
+        Extracts intresting info (name and number of stars)
+        from list of repositories produced by getUrl method
+
+        Parameters:
+            data - data returned by getUrl (List of dictionaries)
+
+        Returns:
+            list of dictionaries, same format as input data
+            but only with important information
+        """
         ret = []
 
         for i in data:
@@ -32,18 +63,37 @@ class github(Resource):
         return ret
 
     @staticmethod
-    def handler(e):
+    def handler(e: Exception) -> typing.Tuple[None, int]:
+        """
+        Handles exceptions thrown by getUrl method
+
+        Parameters:
+            exception of class Exception with string describing the problem
+
+        Returns:
+            None value (as data) and http response code
+        """
         print(e)
         code = 200
         if "Bad username" == str(e):
             code = 400
         elif "User not found" == str(e):
             code = 204
-        return [], code
+        return None, code
 
 
 class List(github):
+    """
+    Class for handling queries for list of repositories
+    """
+
     def get(self):
+        """
+        Method to handle get requests
+
+        Returns:
+            Data (json list of repositories), on exception data provided from exception handler with proper response code
+        """
         args = request.args
         user = args.get("user")
         try:
@@ -54,14 +104,33 @@ class List(github):
 
 
 class Stars(github):
+    """
+    Class for handling queries for total number of stars on repositories
+    """
+
     @staticmethod
-    def starSum(data):
+    def starSum(data: typing.List[typing.Dict]) -> int:
+        """
+        Counts sum of stars in data provided from extractData method
+
+        Parameters:
+            data - data returned by extractData (List of dictionaries)
+
+        Returns:
+            single int - sum of numbers of stars
+        """
         x = 0
         for i in data:
             x += i["stars"]
         return x
 
     def get(self):
+        """
+        Method to handle get requests
+
+        Returns:
+            Data (single int), on exception data provided from exception handler with proper response code
+        """
         args = request.args
         user = args.get("user")
 
